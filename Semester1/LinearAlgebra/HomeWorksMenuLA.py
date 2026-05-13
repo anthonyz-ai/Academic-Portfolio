0e08243fd0f0080a3e8dae6d91228abbd3fe241f
Anthony Narváez <anthonynarvaez59@gmail.com>
2026-05-13 15:57:29 -0500

    feat(math): Add solvers for Cramer, Gauss-Jordan, and parametric systems

--- Semester1/LinearAlgebra/HomeWorksMenuLA.py
+++ Semester1/LinearAlgebra/HomeWorksMenuLA.py
@@ -1,9 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os 
 
 def menuopt():
     while True:
-        clear_screen() 
+        clearScreen() 
         print("======MENU OF LINEAR ALGEBRA PROBLEMS ======")
         print("1. Matrices and vectors exercise")
         print("2. Matrix classification exercise")
@@ -13,38 +14,53 @@ def menuopt():
         print("6. Determinant for Determinant by expansion by cofactors")
         print("7. Calculation of the inverse matrix")
         print("8. Solving a system of linear equiations")
+        print("9. Slope Graph")
+        print("10. Find one, infinite or none solutions of matrix")
+        print("11. Find values of x with cramer")
+        print("12. Find values of x with Gauss-Jordan")
+        print("13. Solution of parametric linear systems")
         print("0. Exit the program")
         option = input("Select the exercise you want: ")
         match option:
             case "1":
-                matrix_vector()
+                matrixVector()
             case "2":
-                matrix_classification()
+                matrixClasificattion()
             case "3":
-                matrix_operations()
+                matrixOperations()
             case "4":
-                matrix_properties()
+                matrixProperties()
             case "5":
-                sarrus_det()
+                sarrusDet()
             case "6":
-                cofactors_det()
+                cofactorsDet()
             case "7":
-                inverse_matrix()
+                inverseMatrix()
             case "8":
-                linear_equiations()
+                linearEquations()
+            case "9":
+                slopeGraph()
+            case "10":
+                oneInfNone()
+            case "11":
+                cramerSolution()
+            case "12":
+                gaussJordanSol()
+            case "13":
+                parametSis()
             case "0":
                 return
             case _:
                 print("Invalid option")
 def main():
     menuopt()
-    clear_screen()
+    clearScreen()
     thankyou()
     pause = input("Press Enter to exit...")
 
-def matrix_vector():
+def matrixVector():
     E = np.array([[13, 17, -9], [17, 5, 2], [-15, -17, 19]]) # Defines matrix E
-    clear_screen()
+    clearScreen()
     print("=====Exercise 1: Matrices and vectors exercise=====")
     print("With matrix E:  ")
     print(E) 
@@ -66,9 +82,9 @@ def matrix_vector():
     print("\033[1;33m" + str(0*EC_3) + "\033[0m\n") # Calculates the linear combination 0*EC_3 and prints it
     pause = input("Press Enter to exit...")
 
-def matrix_classification():
+def matrixClasificattion():
     E = np.array([[13, 17, -9], [17, 5, 2], [-15, -17, 19]]) # Defines matrix E
-    clear_screen()
+    clearScreen()
     print("=====Exercise 2: Matrix classification=====")
     print("With matrix E:  ")
     print("\033[1;33m" + str(E) + "\033[0m\n") # Prints matrix E
@@ -83,8 +99,8 @@ def matrix_classification():
     # 1 indicates that there will be a single column
     pause = input("Press Enter to exit...")
 
-def matrix_operations():
-    clear_screen()
+def matrixOperations():
+    clearScreen()
     A = np.array([[4,3], [-16,-8], [4,0]]) # Defines matrix A 
     B = np.array([[1,2], [3,4], [5,6]]) # Defines matrix B
     print("=====Exercise 3: Matrix operations=====")
@@ -102,8 +118,8 @@ def matrix_operations():
     print("\033[1;33m" + str(A + B + G) + "\033[0m") # Prints the sum of A, B, and G
     pause = input("Press Enter to exit...")
 
-def matrix_properties():
-    clear_screen()
+def matrixProperties():
+    clearScreen()
     B = np.random.randint(-10, 10, size=(2, 3))
     print("=====Exercise 4: Properties of matrix operations=====")
     print("If a matrix product AB results in a column vector, "\
@@ -130,8 +146,8 @@ def matrix_properties():
         print("\033[1;32m" + "The number of columns of A must be equal to the number of rows of B for them to be multiplied" + "\033[0m\n")
     pause = input("Press Enter to exit...")
 
-def sarrus_det():
-    clear_screen()
+def sarrusDet():
+    clearScreen()
     B = np.array([[10,9,-5],[-10,2,9],[6,-5,-6]])
     diagpos = 0
     diagneg = 0
@@ -165,8 +181,8 @@ def expcof(matrix): # This function calculates the determinant of a 3x3 matrix u
         else:
             det3 -= det
     return det3 # Returns the determinant of the minor matrix to the main function
-def cofactors_det():
-    clear_screen()
+def cofactorsDet():
+    clearScreen()
     D = np.array([[-16,-3,-9,-14],[3,7,10,-7],[11,0,4,2],[-18,4,-2,-5]])
     detf = 0
     print("Calculate the determinant of the (4 x 4) matrix D by applying the cofactor expansion method.\
@@ -191,8 +207,8 @@ def cofact(matrix,i,j): # The function returns the cofactors
     C = (menor[0,0] * menor[1,1]) - (menor[0,1] * menor[1,0]) # performs the multiplication and subtraction process 
     C *= ((-1)**(i+j+2)) # multiplies by -1 raised to i+j+2 (+2 because in python matrices start at 0)
     return C # returns the cofactor to the main function
-def inverse_matrix():
-    clear_screen()
+def inverseMatrix():
+    clearScreen()
     V = np.array([[2,8,5],[7,7,15],[4,12,-1]])
     i = 0
     j = 0
@@ -217,8 +233,8 @@ def inverse_matrix():
     print("\033[1;33m" + str(np.round(inV, 4)) + "\033[0m") # np.round(inV,4) rounds the inverse to show 4 decimal places
     pause = input("Press Enter to continue...")
 
-def linear_equiations():
-    clear_screen()
+def linearEquations():
+    clearScreen()
     A = np.array([[1,-2,1,1],[3,1,2,-2],[0,4,-1,-1],[5,0,3,-1]]) # The matrix of scalars
     b = np.array([[2],[-8],[1],[-3]]) # The resulting matrix
     print("Solve the system of linear equations of four equations with four unknowns. Present:")
@@ -245,9 +261,152 @@ def linear_equiations():
     print("\033[1;33m" + str(np.round(x,2)) + "\033[0m\n")
     input("Press Enter to continue...")
 
+def slopeGraph():
+    clearScreen()
+    # Point definition
+    x1,y1 = -15,-12 #Defines P1(-15.-12) on the same line of code
+    x2,y2 = -3,-5 #Defines P1(-3,-5) on the same line of code
+    # Plane limit
+    plt.xlim(min(x1, x2) - 2, max(x1, x2) + 2)  # Adjusts the edges
+    plt.ylim(min(y1, y2) - 2, max(y1, y2) + 2)
+    plt.grid(True) # It is True so that the grid is visible
+    # Title and descriptions
+    plt.xlabel("X Axis",fontweight="bold") # Adds X axis 
+    plt.ylabel("Y Axis",fontweight="bold") # Adds Y axis 
+    plt.title("Line Graph: Exercise 1") # Adds the title to the graph
+    # Line graph
+    plt.plot([x1,x2],[y1,y2],"g-",linewidth=2) # Graphs the green line with a width of 2
+    # Datatips, allows observing the coordinates
+    plt.annotate(f"P1({x1},{y1})", (x1, y1), textcoords="offset points", xytext=(0,-13), ha="center")
+    plt.annotate(f"P2({x2},{y2})", (x2, y2), textcoords="offset points", xytext=(0,10), ha="center")
+    # Slope calculation
+    slope = (y2 - y1) / (x2 - x1)
+    xm = (x1 + x2) / 2
+    ym = (y1 + y2) / 2
+    plt.annotate(f'm = {slope:.2f}', # Prints the slope value
+            xy=(xm, ym), # The coordinates where the arrow will point
+             xytext=(-14, -7), # The coordinates where the coordinates will be printed
+             arrowprops=dict(arrowstyle="->", color="red"), # The characteristics of the arrow
+             fontsize=10, color="red") # The characteristics with which the slope value will be printed
+    plt.show()
+    input("Press Enter to continue...")
 
-    
-def clear_screen():
+def searchsol(A,b): # It is done in a different function to make it reusable 
+    if len(A) < len(b): # If the system has more unknowns (columns) than equations (rows)
+        print("The system has infinite solutions, because it has more unknowns than equations")
+    else:
+        j = 0
+        for i in range(3): # If the system has a null row and the result is different from 0, it has no solution
+            if np.all(A[i] == 0) and b[i] != 0:
+                j += 1
+        if j > 0:
+            print("The system has no solutions, because one of its rows is null and gives a result different from 0.")
+        else: # If none of the above cases occur, it has a unique solution
+            print("The system has a unique solution")
+def oneInfNone():
+    clearScreen()
+    A = np.array([[1,0,0],[0,1,2],[0,0,0]]) # Definition of matrix A
+    b = np.array([[0],[0],[1]]) # Definition of the matrix of independent terms
+    Ab = np.hstack((A,b)) # Joins the two matrices to form the augmented one
+    print("Matrix A is:")
+    print("\033[1;33m"+str(A)+"\033[0m\n")
+    print("The vector of independent terms is: ")
+    print("\033[1;33m"+str(b)+"\033[0m\n")
+    print("The augmented matrix is: ")
+    print("\033[1;33m"+str(Ab)+"\033[0m\n")
+    searchsol(A,b)
+    input("Press Enter to continue...")
+
+def cramerMeth(A,b,detA): # It is in a separate function to be reusable
+    if detA == 0: # If the determinant is equal to 0, Sarrus's method cannot be applied
+        print("Sarrus's method cannot be applied, because the determinant is 0")
+    for i in range(3): # Loop for the 3 columns
+        Sarr = A.copy() # Saves in Sarr a copy of matrix A to avoid overwriting it
+        Sarr[:,i] = b.flatten() # Changes in Sarr the column according to i for the matrix of independent terms b
+        detS = np.linalg.det(Sarr) # Calculates the matrix quickly
+        print(f"x{i+1} is equal to: {round(detS / detA, 2)}") # For each unknown shows the result
+def cramerSolution():
+    clearScreen()
+    A = np.array([[1,1,2],[2,4,-3],[3,6,-5]])
+    b = np.array([[9],[1],[0]])
+    detA = round(np.linalg.det(A),2) # np.linalg.det() a method to get the determinant quickly
+    print("Matrix A is :")
+    print("\033[1;33m"+str(A)+"\033[0m\n")
+    print("The vector of independent terms is: ")
+    print("\033[1;33m"+str(b)+"\033[0m\n")
+    print("The determinant of matrix A is equal to: "+"\033[1;33m"+str(detA)+"\033[0m\n")
+    cramerMeth(A,b,detA) # Goes to the function cramerMeth with the matrix A,b and the determinant of A
+    input("Press Enter to continue...")
+
+def gJordanMeth(Ab):
+    for i in range(3): # Forms the upper triangular matrix
+        if Ab[i,i] != 1: # If the element of the main diagonal is different from 1
+            Ab[i] /= Ab[i,i] # Divides the entire row by the element
+            for j in range(i+1,3): # Subtracts the elements below the pivot
+                if Ab[j,i] != 0: # If it is different from 0
+                    Ab[j] = Ab[j] - Ab[i]*Ab[j,i] # Subtracts the row by the pivot row multiplied by the element below
+        else: # Otherwise continues normally
+            for j in range(i+1,3): # Subtracts the elements below the pivot
+                if Ab[j,i] != 0: # If it is different from 0
+                    Ab[j] = Ab[j] - Ab[i]*Ab[j,i] # Subtracts the row by the pivot row multiplied by the element below
+    for i in range(2,0,-1): # Forms the identity matrix 
+        for j in range(i-1,-1,-1):
+            Ab[j] = Ab[j] - Ab[i]*Ab[j,i] #Eliminates the elements from bottom to top using the ones already formed
+    print("Solved matrix:")
+    print("\033[1;33m"+str(Ab)+"\033[0m\n")
+    print(f"x1 is equal to: {Ab[0,3]}")
+    print(f"x2 is equal to: {Ab[1,3]}")
+    print(f"x3 is equal to: {Ab[2,3]}")
+def gaussJordanSol():
+    clearScreen()
+    A = np.array([[2,4,6],[4,5,6],[3,1,-2]], dtype=float) # Matrix A is defined with decimal numbers to be able to perform calculations
+    b = np.array([[0],[0],[0]], dtype=float) # The matrix of independent terms is defined in the same way
+    Ab = np.hstack((A,b)) # Joins the two matrices to form the augmented one
+    print("The matrix is: ")
+    print("\033[1;33m"+str(A)+"\033[0m\n")
+    print("The vector of independent terms is: ")
+    print("\033[1;33m"+str(b)+"\033[0m\n")
+    print("The augmented matrix is:")
+    print("\033[1;33m"+str(Ab)+"\033[0m\n")
+    gJordanMeth(Ab) # Takes the augmented matrix to apply Gauss Jordan 
+    input("Press Enter to continue...")
+
+def freevar(A):
+    pivots = []
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            if A[i, j] != 0:
+                pivots.append(j)
+                break   
+    for j in range(A.shape[1]):
+        if j in pivots:
+            print(f"x{j+1} is a basic variable ")
+        else:
+            print(f"x{j+1} is a free variable ")
+
+def parametSis():
+    clearScreen()
+    A = np.array([[1,0,8,-5],[0,1,4,-9],[0,0,1,1]])
+    b = np.array([[6],[3],[2]])
+    Ab = np.hstack((A,b))
+    print("The linear system is:  ")
+    print("\033[1;33m"+str(Ab)+"\033[0m\n")
+    print("a) Is the system consistent? Does it have a unique solution or infinitely many solutions? Explain your answers.")
+    searchsol(A,b)
+    print("b) Which variables are basic and which ones are free?")
+    freevar(A)
+    print("c) Set up the equations for the parametric solution with the basic variables being functions of the free variables?")
+    print("The equations are: \nx4 = t \nx3 = 2 - t \nx2 = -5 + 13t \nx1 = -10 + 13t")
+    print("d) Set up the equations for the parametric solution with the basic variables being functions of the free variables")
+    t = int(input("Enter a value for t: "))
+    x4 = t
+    x3 = 2 - t
+    x2= -5 + 13*t
+    x1 = -10 + 13*t
+    print(f"When t is equal to {t}: \nx4 = {t} \nx3 = {x3} \nx2={x2} \nx1 = {x1}")
+    input("Press Enter to exit...")
+
+def clearScreen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def thankyou():
